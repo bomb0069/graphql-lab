73403d87d6dfb7b5d08b21ffe7fe3540bc4b964b
karan sivarat <bomb0069@gmail.com>
2024-06-01 10:47:12 +0700

    add contact resolver

---
+++ pkg/graphql/resolvers/contact.resolver.go
@@ -0,0 +1,41 @@
+package resolvers
+
+import (
+	"fmt"
+	"graphql-api/internal/contact"
+
+	"github.com/graphql-go/graphql"
+)
+
+func GetContactResolve(params graphql.ResolveParams) (interface{}, error) {
+	// Get the query from the context
+	query, _ := params.Context.Value("GraphQLQuery").(string)
+
+	fmt.Printf("GraphQL Query: %s", query)
+
+	// Update limit and offset if provided
+	limit, ok := params.Args["limit"].(int)
+	if !ok {
+		limit = 10
+	}
+
+	offset, ok := params.Args["offset"].(int)
+	if !ok {
+		offset = 0
+	}
+
+	searchText, ok := params.Args["searchText"].(string)
+	if !ok {
+		searchText = ""
+	}
+	contactRepo := contact.NewContactRepo()
+
+	// Fetch contacts from the database
+	contacts, err := contactRepo.GetContactsBySearchText(searchText, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	// time.Sleep(1 * time.Minute)
+	
+	return contacts, nil
+}
