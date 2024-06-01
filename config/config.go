40ea9e57f599b77684a12db18d74b13f69bf8b0f
karan sivarat <bomb0069@gmail.com>
2024-06-01 11:24:49 +0700

    add config and cmd

--- config/config.go
+++ config/config.go
@@ -2,66 +2,98 @@ package config
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"path/filepath"
 	"sync"
 
 	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 // Config represents application configuration
 type Config struct {
-    DBHost     string
-    DBPort     int
-    DBUser     string
-    DBPassword string
-  DBName string
-    SecretKey string
-    TokenAge int
+	DBHost          string
+	DBPort          int
+	DBUser          string
+	DBPassword      string
+	DBName          string
+	SecretKey       string
+	TokenAge        int
+	GraphQLPort     int
+	LogServerPort   int
+	LogMergeMin     int
+	LogMoveMin      float64
+	RateLimitReqSec int
+	RateLimitBurst  int
+	CacheProvider   int
+	CacheConnString string
+	CachePassword   string
+	CacheIndex      int
+	CacheAge        int
 }
+
+const (
+	DBHost          = "DB_HOST"
+	DBPort          = "DB_PORT"
+	DBUser          = "DB_USER"
+	DBPassword      = "DB_PASSWORD"
+	DBName          = "DB_NAME"
+	SecretKey       = "SECRETE_KEY"
+	TokenAge        = "TOKEN_AGE"
+	GraphQLPort     = "GRAPHQL_PORT"
+	LogServerPort   = "LOG_SERVER_PORT"
+	LogMergeMin     = "LOG_MERGE_MIN"
+	LogMoveMin      = "LOG_MOVE_MIN"
+	RateLimitReqSec = "RATE_LIMIT_REQ_SEC"
+	RateLimitBurst  = "RATE_LIMIT_BURST"
+	CacheAge        = "CACHE_AGE"
+	CacheConnString = "CACHE_CONNECTION_STRING"
+	CacheIndex      = "CACHE_INDEX"
+	CachePassword   = "CACHE_PASSWORD"
+)
 
 var instance *Config
 var once sync.Once
 
 // LoadConfig loads the configuration from environment variables
 func NewConfig() *Config {
-    once.Do(func() {
-        // Load environment variables from .env file
-        if err := godotenv.Load(); err != nil {
-            fmt.Println("Failed to load env variables:", err)
-            return
-        }
+	once.Do(func() {
+		relativePath := "./config/.env"
 
-        // Parse environment variables and create config object
-        instance = &Config{
-            DBHost:     os.Getenv("DB_HOST"),
-            DBUser:     os.Getenv("DB_USER"),
-            DBPassword: os.Getenv("DB_PASSWORD"),
-      DBName: os.Getenv("DB_NAME"),
-            SecretKey: os.Getenv("SECRET_KEY"),
-        }
+		// Get the absolute path
+		absolutePath, err := filepath.Abs(relativePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Load environment variables from .env file
+		if err := godotenv.Load(absolutePath); err != nil {
+			fmt.Println("Failed to load env variables:", err)
+			return
+		}
 
-        // Parse DB port as integer
-        dbPortStr := os.Getenv("DB_PORT")
-        if dbPortStr != "" {
-            dbPort, err := strconv.Atoi(dbPortStr)
-            if err != nil {
-                fmt.Println("Invalid DB_PORT value:", err)
-                return
-            }
-            instance.DBPort = dbPort
-        }
-        tokenAge, err :=strconv.Atoi(os.Getenv("TOKEN_AGE"))
-        if (err != nil) {
-            fmt.Println("Invalid TOKEN_AGE value:", err)
-            return
-        }
-        instance.TokenAge = tokenAge
-    })
-    return instance
+		viper.AutomaticEnv()
+
+		// Create a Config instance and set values from Viper
+		instance = &Config{
+			DBHost:          viper.GetString(DBHost),
+			DBPort:          viper.GetInt(DBPort),
+			DBUser:          viper.GetString(DBUser),
+			DBPassword:      viper.GetString(DBPassword),
+			DBName:          viper.GetString(DBName),
+			SecretKey:       viper.GetString(SecretKey),
+			TokenAge:        viper.GetInt(TokenAge),
+			GraphQLPort:     viper.GetInt(GraphQLPort),
+			LogServerPort:   viper.GetInt(LogServerPort),
+			LogMergeMin:     viper.GetInt(LogMergeMin),
+			LogMoveMin:      viper.GetFloat64(LogMoveMin),
+			RateLimitReqSec: viper.GetInt(RateLimitReqSec),
+			RateLimitBurst:  viper.GetInt(RateLimitBurst),
+		}
+	})
+	return instance
 }
 
 // GetConfig returns the singleton configuration instance
 func GetConfig() *Config {
-    return instance
+	return instance
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/graphql-go/handler v0.2.4 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/spf13/viper v1.18.2 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 )
